b3aab34aac80911229bd44061211ea5c783ba56c
Julius Polar <julius.polar@protonmail.com>
2024-07-17 16:25:25 +0200

    updated storybook version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,9 +10,7 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
   core: {
     disableTelemetry: true,
   },
