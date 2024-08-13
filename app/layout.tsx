c7b624182cac93b1c901a4ece1923cf11d67a7e1
Julius Polar <julius.polar@protonmail.com>
2024-08-13 14:29:54 +0200

    dont use next metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
-import type { Metadata } from "next";
+//import type { Metadata } from "next";
 import dynamic from "next/dynamic";
 
 import "@julianelda/scratchpad/style.css";
 import "./../styles/index.css";
 import "./tailwind.css";
 
+/*
 export const metadata: Metadata = {
   title: "meter",
   description: "meter",
 };
+*/
 
 const AppComponent = dynamic(() => import("./../src/components/App/App"), {
   ssr: false,
@@ -21,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <base href="/" />
+      </head>
       <body className="bg-gray-50 p-2 font-serif text-neutral-900 dark:bg-gray-800 dark:text-gray-50">
         <AppComponent>{children}</AppComponent>
       </body>
