db32c5276e58ef326b330a5f783da7f41107df26
Julius Polar <julius.polar@protonmail.com>
2025-04-22 13:48:03 +0200

    use next font package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 import App from "./../src/components/App/App";
-
+import localFont from "next/font/local";
 import "./tailwind.css";
 
 export const metadata: Metadata = {
@@ -9,13 +9,63 @@ export const metadata: Metadata = {
   description: "meter",
 };
 
+const heliotrope3 = localFont({
+  src: [
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_3_regular.woff2",
+      style: "normal",
+    },
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_3_italic.woff2",
+      style: "italic",
+    },
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_3_bold.woff2",
+      weight: "bold",
+    },
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_3_bold_italic.woff2",
+      style: "italic",
+      weight: "bold",
+    },
+  ],
+  display: "swap",
+  variable: "--font-heliotrope3",
+});
+
+const heliotrope4 = localFont({
+  src: [
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_4_regular.woff2",
+      style: "normal",
+    },
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_4_italic.woff2",
+      style: "italic",
+    },
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_4_bold.woff2",
+      weight: "bold",
+    },
+    {
+      path: "./../node_modules/@julianelda/stilo/fonts/heliotrope_4_bold_italic.woff2",
+      style: "italic",
+      weight: "bold",
+    },
+  ],
+  display: "swap",
+  variable: "--font-heliotrope4",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${heliotrope3.variable} ${heliotrope4.variable}`}>
       <body className="bg-gray-50 p-2 font-serif text-neutral-900 dark:bg-gray-800 dark:text-gray-50">
         <App>{children}</App>
       </body>
