96e8b7635b67dfe9c095b72efbaae5edfb19ab73
Julius Polar <julius.polar@protonmail.com>
2025-05-01 08:38:53 +0200

    added basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: isProd ? "/meter" : "",
+  assetPrefix: isProd ? "/meter/" : "",
+  trailingSlash: true,
+};
 
 export default nextConfig;
