e309099da858e31e32b923b489476ca89ebb7118
Julius Polar <julius.polar@protonmail.com>
2025-06-12 15:50:30 +0200

    update footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,8 +5,9 @@ import { Footer as FooterLib } from "@julianelda/scratchpad";
 export function Footer() {
   return (
     <FooterLib
-      label="Julius Polar@GitHub"
+      label="Julius Polar"
       link="https://github.com/JulianElda/meter"
+      extraClass="max-w-4xl"
     />
   );
 }
