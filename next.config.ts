515427e194f2fb2e839d26e4ea504fe1ca7d62ab
Julius Polar <julius.polar@protonmail.com>
2025-06-12 15:14:27 +0200

    deploy to base url

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/meter" : "",
-  assetPrefix: isProd ? "/meter/" : "",
   trailingSlash: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
