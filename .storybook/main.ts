1754af98fdfdc1f60b59ff15382f864bc5627ca3
Julius Polar <julius.polar@protonmail.com>
2025-06-24 13:51:40 +0200

    added storybook test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
   stories: ["../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-a11y", "@storybook/addon-themes"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-themes",
+    "@storybook/addon-vitest"
+  ],
   framework: {
     name: "@storybook/nextjs-vite",
     options: {},
---
+++ app/area/area.test.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { expect } from "storybook/test";
+import { Area } from "./area";
+import { AreaUnits } from "@/src/constants/area";
+
+const meta: Meta<typeof Area> = {
+  title: "Tests/Area",
+  component: Area,
+  decorators: [
+    (Story) => (
+      <div className="max-w-md text-lg">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof Area>;
+
+export const TestElements: Story = {
+  name: "render elements",
+  play: async ({ canvas }) => {
+    await expect(canvas.getByTestId("from-input")).toBeInTheDocument();
+    await expect(canvas.getByTestId("from-select")).toBeInTheDocument();
+    await expect(canvas.getByTestId("result-input")).toBeInTheDocument();
+    await expect(canvas.getByTestId("result-select")).toBeInTheDocument();
+  },
+};
+
+export const AreaPageTest: Story = {
+  name: "change value and units",
+  play: async ({ canvas, userEvent }) => {
+    await userEvent.clear(canvas.getByTestId("from-input"));
+    await userEvent.type(canvas.getByTestId("from-input"), "1.5");
+
+    await userEvent.selectOptions(
+      canvas.getByTestId("from-select"),
+      AreaUnits.m
+    );
+    await userEvent.selectOptions(
+      canvas.getByTestId("result-select"),
+      AreaUnits.yard
+    );
+
+    // 1.5 m² -> 1.794 yard²
+    await expect(canvas.getByTestId("result-input")).toHaveValue("1.794");
+
+    // change result units
+    // 1.5 m² -> 16.146 ft²
+    await userEvent.selectOptions(
+      canvas.getByTestId("result-select"),
+      AreaUnits.ft
+    );
+    await expect(canvas.getByTestId("result-input")).toHaveValue("16.146");
+
+    // change from units
+    // 1.5 Acre -> 65340 ft²
+    await userEvent.selectOptions(
+      canvas.getByTestId("from-select"),
+      AreaUnits.acre
+    );
+    await expect(canvas.getByTestId("result-input")).toHaveValue("65340");
+  },
+};
